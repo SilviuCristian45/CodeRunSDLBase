cd5b5f1c639a9a12f48965faa8b4593c2499e93f
DESKTOP-LDQLJQC\Silviu <silviudinca412@gmail.com>
2021-04-20 11:57:01 +0300

    cod cum trebe

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <glm/glm/glm.hpp>
+#include "testbun/circle.h"
 
 #include <iostream>
 
@@ -12,9 +13,26 @@ SDL_Renderer *windowRenderer;
 SDL_Event currentEvent;
 
 bool quit = false;
+bool animationQuit = false;
 int mouseX, mouseY;
 
 glm::vec4 backgroundColor;
+glm::vec2 point1, point2;
+
+Circle circleEmpty;
+Circle circle;
+
+Circle circleGravity;
+
+SDL_Rect ground;
+
+bool right = true;
+
+float elapsedTime,speed = 50.0f;
+
+const float MASS = 50.0f;
+const float acceleration = 10.0f;
+bool isOnGround = false;
 
 bool initWindow() {
 
@@ -28,7 +46,7 @@ bool initWindow() {
     } else {
 
         // Try to create the window
-        window = SDL_CreateWindow("SDL2 Hello Window",
+        window = SDL_CreateWindow("Fereastra de test",
                                   SDL_WINDOWPOS_UNDEFINED,
                                   SDL_WINDOWPOS_UNDEFINED,
                                   WINDOW_WIDTH,
@@ -68,6 +86,28 @@ void destroyWindow() {
 void initScene() {
 
     backgroundColor = glm::vec4(255.0f, 255.0f, 255.0f, 255.0f);
+    //initializam cercul
+    //circleEmpty.center.x = 100.0f;
+    //circleEmpty.center.y = 100.0f;
+    //circleEmpty.radius = 25.0f;
+    //circleEmpty.color = glm::vec4(100.0f,100.0f,100.0f,255.0f);
+
+    circle.center.x = 200.0f;
+    circle.center.y = 200.0f;
+    circle.radius = 25.0f;
+    circle.color = glm::vec4(100.0f,100.0f,100.0f,255.0f);
+
+    //init the circle for gravity test
+    circleGravity.center.x = 350.0f;
+    circleGravity.center.y = 350.0f;
+    circleGravity.radius = 45.0f;
+    circle.color = glm::vec4(50.0f,25.0f,18.0f,255.0f);
+
+    //init the ground
+    ground.x = 150;
+    ground.y = 700;
+    ground.w = 800;
+    ground.h = 150;
 }
 
 void processEvent(SDL_Event event) {
@@ -85,6 +125,11 @@ void processEvent(SDL_Event event) {
 
             SDL_GetMouseState(&mouseX, &mouseY);
             std::cout << "Left mouse button pressed at (" << mouseX << ", " << mouseY << ")" << std::endl;
+            //stergem linia anterioara
+            //point1.x = point1.y = point2.x = point2.y = 0.0f;
+            //setam coordonatele la mouseX mouseY
+            point1.x = point2.x =  static_cast<float> (mouseX);
+            point1.y = point2.y = static_cast<float> (mouseY);
         }
 
         if(event.button.button == SDL_BUTTON_RIGHT) {
@@ -94,6 +139,10 @@ void processEvent(SDL_Event event) {
         }
     }
 
+    if(event.type == SDL_MOUSEBUTTONUP){
+        std::cout<<" mouse button up";
+    }
+
     // Mouse event -> mouse movement
     if(event.type == SDL_MOUSEMOTION) {
 
@@ -101,6 +150,8 @@ void processEvent(SDL_Event event) {
 
             SDL_GetMouseState(&mouseX, &mouseY);
             std::cout << "Left mouse drag at (" << mouseX << ", " << mouseY << ")" << std::endl;
+            point2.x = mouseX;
+            point2.y = mouseY;
         }
 
         if(SDL_GetMouseState(nullptr, nullptr) & SDL_BUTTON_RMASK) {
@@ -120,6 +171,8 @@ void processEvent(SDL_Event event) {
                 break;
             case SDLK_r:
                 std::cout << "R key pressed" << std::endl;
+                animationQuit = !animationQuit;//oprim animatia sau o pornim
+                std::cout << "animation is fking " << animationQuit << "\n";
                 break;
 
             default:
@@ -128,8 +181,43 @@ void processEvent(SDL_Event event) {
     }
 }
 
-void drawFrame() {
+void animate(){
+    if(right){//daca mergem la dreapta
+        if (circle.center.x <= 400.0f)//daca nu am ajuns
+            circle.center.x += (50.0f * elapsedTime);
+        else//daca am ajuns
+            right = false;
+    }
+    else {//daca mergem la stanga
+        if(circle.center.x >= 100.0f)//daca nu am ajuns la punctul din stanga
+            circle.center.x -= (50.0f * elapsedTime);
+        else //daca am ajuns
+            right = true;
+    }
 
+    //for(int i = 0; i < 100000000; i++);
+}
+
+void animate2(){
+    circle.center = circle.center + glm::normalize(point2 - circle.center) * speed * elapsedTime;
+}
+
+void gravity(){
+    //luam distanta intre cerc si ground .
+    glm::vec2 groundPosition(ground.x, ground.y);
+    float distance = glm::distance(circle.center, groundPosition );
+
+
+    if (!isOnGround && circle.center.y <= ground.y){//daca nu e pe pamant , il aducem pe pamant
+        float greutate = (MASS * acceleration) / distance;
+        circle.center.y += greutate;// impartim cu distanta
+        std::cout << "distance : " << distance << " viteza de cadere" << greutate << std::endl;
+    }
+    else isOnGround = true;
+}
+
+void drawFrame() {
+    Uint32 start = SDL_GetTicks();
     // clear background
     SDL_SetRenderDrawColor(windowRenderer,
                            static_cast<Uint8>(backgroundColor.r),
@@ -140,9 +228,23 @@ void drawFrame() {
 
     // draw the scene
     //TODO
+    //SDL_SetRenderDrawColor(windowRenderer,0,0,0,255);
+    //SDL_RenderDrawLine(windowRenderer,point1.x,point1.y,point2.x,point2.y);
+    //SDL_DrawCircle(windowRenderer, circleEmpty);
+    SDL_FillCircle(windowRenderer, circle);
+    SDL_FillCircle(windowRenderer, circleGravity);
 
+    SDL_SetRenderDrawColor(windowRenderer, 100, 30, 80, 255);
+    SDL_RenderFillRect(windowRenderer, &ground);
+
+    if(animationQuit)
+        animate2();
+
+    gravity();
     // render to window
     SDL_RenderPresent(windowRenderer);
+    Uint32 end = SDL_GetTicks();
+    elapsedTime = (end-start)/1000.0f;//convertim in milisecunde
 }
 
 int main(int argc, char* args[]) {
